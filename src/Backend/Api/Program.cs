e5c158e455ebe6137891d058fb6d4253dd26910c
Maxime Poiron <mpoiron@expertime.com>
2025-07-18 12:05:57 +0200

    feat: generate client api with kiota

--- src/Backend/Api/Program.cs
+++ src/Backend/Api/Program.cs
@@ -1,4 +1,6 @@
 using Backend.Api.Data.Helpers;
+using FastEndpoints.ClientGen.Kiota;
+using Kiota.Builder;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,19 +8,26 @@ builder.AddApiServices();
 
 var app = builder.Build();
 
-if (builder.Environment.IsDevelopment())
-{
-    using var scope = app.Services.CreateScope();
-    await scope.ServiceProvider.GetRequiredService<DbInitializer>().RunAsync();
-}
-
 app.UseFastEndpoints();
+await app.GenerateApiClientsAndExitAsync(
+    c =>
+    {
+        c.SwaggerDocumentName = "v1";
+        c.Language = GenerationLanguage.TypeScript;
+        c.OutputPath = Path.Combine(app.Environment.ContentRootPath, "../../Frontend/src/clients/apiClients");
+        c.ClientNamespaceName = "Frontend";
+        c.ClientClassName = "ApiClient";
+    });
+
 app.MapDefaultEndpoints();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseOpenApi(cfg => cfg.Path = "/openapi/{documentName}.json");
     app.MapScalarApiReference();
+
+    using var scope = app.Services.CreateScope();
+    await scope.ServiceProvider.GetRequiredService<DbInitializer>().RunAsync();
 }
 
 await app.RunAsync();
